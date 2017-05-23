352bc931ca88fc47a66e91d26029f0e11ee9af8c
eileen <eileen@eileen.com>
2017-05-23 17:03:47 +0200

    english translation

--- js/scripts.js
+++ js/scripts.js
@@ -2,32 +2,31 @@ var iPhone6S,
 	galaxy,
 	onePlus;
 
-function Telefon(marka, cena, kolor, rozdz) { 
-	 this.marka = marka; 
-	 this.cena = cena; 
-	 this.kolor = kolor;
-	 this.rozdz = rozdz;
+function Phone(brand, price, color, screen) { 
+	 this.brand = brand; 
+	 this.price = price; 
+	 this.color = color;
+	 this.screen = screen;
 }
 
-Telefon.prototype.printInfo = function() { 
-	console.log("Marka telefonu to " + this.marka + ", kolor to " + this.kolor + ", a cena to " + this.cena + "."); 
+Phone.prototype.printInfo = function() { 
+	console.log("The phone brand is " + this.brand + ", the color is " + this.color + ", and price is " + this.price + "."); 
 }
 
-Telefon.prototype.printRozdz = function() { 
-	console.log("Rozdzielczość ekranu telefonu " + this.marka + " to " + this.rozdz + "."); 
+Phone.prototype.printScreen = function() { 
+	console.log("The " + this.brand + " phone screen size is " + this.screen + "."); 
 }
 
-
-iPhone6S = new Telefon("Apple", 2250, "srebrny", "7\"");
-galaxy = new Telefon("Samsung", 1250, "złoty", "9\"");
-onePlus = new Telefon("OnePlus One", 250, "biały", "8\"");
+iPhone6S = new Phone("Apple", 2250, "silver", "7\"");
+galaxy = new Phone("Samsung", 1250, "gold", "9\"");
+onePlus = new Phone("OnePlus One", 250, "white", "8\"");
 
 iPhone6S.printInfo();
 galaxy.printInfo();
 onePlus.printInfo();
 
-iPhone6S.printRozdz();
-galaxy.printRozdz();
-onePlus.printRozdz();
+iPhone6S.printScreen();
+galaxy.printScreen();
+onePlus.printScreen();
 
 
